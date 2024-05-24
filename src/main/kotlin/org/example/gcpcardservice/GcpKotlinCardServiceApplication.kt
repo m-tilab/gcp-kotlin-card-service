99e2a0247472c7fce952ab6cf5ed5655e42237a0
mehdi.tilab <mehdi.tilab@virtudev.net>
2024-05-24 10:08:39 +0200

    Initial commit

---
+++ src/main/kotlin/org/example/gcpcardservice/GcpKotlinCardServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.example.gcpcardservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GcpKotlinCardServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GcpKotlinCardServiceApplication>(*args)
+}
